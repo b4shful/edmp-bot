8ee51d159a448bb970bf3a52cf12268abe973ccf
RobertRandolph (mbp) <audiolabs@gmail.com>
2018-02-21 12:15:20 -0500

    match against lower case, doh!

--- filters/collab.js
+++ filters/collab.js
@@ -10,7 +10,7 @@ exports.run = async (client, message) => {
     var idiotValidation = true;
 
     for (var i = 0; i < tags.length; i++) {
-	if (!message.content.includes(tags[i])) {
+	if (!message.content.toLowerCase().includes(tags[i])) {
 	    missing.push(tags[i]);
 	}
     }
