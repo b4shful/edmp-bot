eb96c0c6396b91cb37e4cf213e1d369fd85abdc1
Brian Uosseph <brian.uosseph@gmail.com>
2018-03-19 23:42:08 -0400

    Fix typo in welcome message

--- events/guildMemberAdd.js
+++ events/guildMemberAdd.js
@@ -1,7 +1,7 @@
 const Logger = require('../util/Logger');
 
 const welcomeMessage = (member, rulesChannel = '`#rules_and_how-to`') =>
-	`Welcome to EDMP ${member}! Take a moment to read ${rulesChannel}, then tell \`@Staff\` what DAW you use to get access to the dozens of other channesl we have available. Please be patient as staff may not be immediately available to give you your role(s), they sleep too!\n\nDon't produce? That's fine! Tell us if you're a vocalist, graphic artist, or just a music fan and you'll get access.`;
+	`Welcome to EDMP ${member}! Take a moment to read ${rulesChannel}, then tell \`@Staff\` what DAW you use to get access to the dozens of other channels we have available. Please be patient as staff may not be immediately available to give you your role(s), they sleep too!\n\nDon't produce? That's fine! Tell us if you're a vocalist, graphic artist, or just a music fan and you'll get access.`;
 
 /**
  * This event executes when a new member joins a server.
