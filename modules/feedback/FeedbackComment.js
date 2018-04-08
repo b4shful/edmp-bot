9bfb45c7d8ebe9b0971a22b5bb6e86e42c408f5c
Brian Uosseph <brian.uosseph@gmail.com>
2018-04-08 14:31:12 -0400

    Add FeedbackComment to feedback module

---
+++ modules/feedback/FeedbackComment.js
@@ -0,0 +1,61 @@
+/**
+ * A feedback comment is a message made in response to someone's feedback
+ * request. It requires a reference to the feedback request, and should
+ * reward a feedback point to the user making the comment.
+ */
+const Logger = require('../../util/Logger');
+
+const logQuery = (query, parameters) =>
+	Logger.log(`Database operation:\n${query.trim()}\n${JSON.stringify(parameters, undefined, 2)}\n`);
+
+/**
+ * @param {Database} database
+ * @param {string} requestId
+ * @throws If a FeedbackRequest with `requestId` does not exist
+ * @throws If execution of database statement fails
+ */
+const confirmRequestExists = (database, requestId) => {
+	const SELECT_REQUEST = `SELECT * FROM FeedbackRequest WHERE id = $requestId`;
+	const selectParameters = { requestId };
+
+	logQuery(SELECT_REQUEST, selectParameters);
+	const request = database.prepare(SELECT_REQUEST).get(selectParameters);
+
+	if (!request) {
+		throw new TypeError('Must reference an existing feedback request.');
+	}
+};
+
+/**
+ * @param {Database} database
+ * @param {string} requestId
+ * @param {string} userId
+ * @param {string} message
+ * @throws If a FeedbackRequest with `requestId` does not exist
+ * @throws If execution of database statement fails
+ */
+exports.create = (database, requestId, userId, message) => {
+	if (!database) {
+		throw new TypeError('Expected a database connection.');
+	}
+
+	if (!requestId || typeof requestId !== 'string') {
+		throw new TypeError('A FeedbackComment requires a requestId string');
+	}
+
+	if (!userId || typeof userId !== 'string') {
+		throw new TypeError('A FeedbackComment requires a userId string');
+	}
+
+	if (!message || typeof message !== 'string') {
+		throw new TypeError('A FeedbackComment requires a message string');
+	}
+
+	confirmRequestExists(database, requestId);
+
+	const INSERT_COMMENT = `INSERT INTO FeedbackComment (requestId, userId, message) VALUES ($requestId, $userId, $message)`;
+	const parameters = { requestId, userId, message };
+
+	logQuery(INSERT_COMMENT, parameters);
+	database.prepare(INSERT_COMMENT).run(parameters);
+};
