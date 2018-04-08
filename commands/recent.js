356e02421d28e4ba7b2a847b5b9f7514a72776d6
Brian Uosseph <brian.uosseph@gmail.com>
2018-04-08 19:30:21 -0400

    Update recent command to use new feedback module

--- commands/recent.js
+++ commands/recent.js
@@ -1,3 +1,29 @@
+const Logger = require('../util/Logger');
+const FeedbackRequest = require('../modules/feedback/FeedbackRequest');
+
+const formatRequestList = (members, requests) => {
+	const list = requests.map(request => {
+		try {
+			const link = FeedbackRequest.getLink(request);
+			const displayName = members.get(request.userId).displayName;
+			return `- \`${request.id}\` ${displayName} - <${link}>`;
+		}
+		catch (error) {
+			Logger.error(error);
+			return '';
+		}
+	}).filter(item => item.length !== 0);
+
+	if (!(list.length > 0)) {
+		return 'No one has requested feedback yet!';
+	}
+
+	return [
+		'Recent feedback requests:',
+		...list
+	].join('\n');
+};
+
 /**
  * 
  * @param {Discord.Client} client The Discord API client
@@ -6,29 +32,31 @@
  * @param {number} level The permission level of the author of the message
  */
 exports.run = (client, message) => {
-  if (message.author.bot) return;
+	if (message.author.bot) return;
 
-  if (message.channel.name !== 'feedback-trade') {
-    const feedbackChannel =
-      message.guild.channels.find('name', 'feedback-trade') || '#feedback-trade';
+	if (message.channel.name !== 'feedback-trade') {
+		const feedbackChannel =
+			message.guild.channels.find('name', 'feedback-trade') || '#feedback-trade';
 
-    message.channel.send(`\`recent\` only works in ${feedbackChannel}.`);
-    return;
-  };
+		message.channel.send(`\`recent\` only works in ${feedbackChannel}.`);
+		return;
+	};
 
-  message.channel.send('**TODO:** Get the last 6 links from people asking for feedback');
+	const members = message.guild.members;
+	const requests = FeedbackRequest.recent(client.database, 6);
+	message.channel.send(formatRequestList(members, requests));
 }
 
 exports.conf = {
-  enabled: true,
-  guildOnly: true,
-  aliases: [],
-  permLevel: 'User'
+	enabled: true,
+	guildOnly: true,
+	aliases: [],
+	permLevel: 'User'
 };
 
 exports.help = {
-  name: 'recent',
-  category: 'Feedback',
-  description: '',
-  usage: 'recent'
+	name: 'recent',
+	category: 'Feedback',
+	description: '',
+	usage: 'recent'
 };
