80569214ab6ef4af8c6bde8666827086065eaa6d
Brian Uosseph <brian.uosseph@gmail.com>
2018-04-23 22:25:20 -0400

    Add metion support to points command

--- commands/points.js
+++ commands/points.js
@@ -1,6 +1,29 @@
 const FeedbackPoint = require('../modules/feedback/FeedbackPoint');
 const feedbackUsage = require('./feedback').help.usage;
 
+const getMentionedMember = message => {
+	const { author, mentions } = message;
+	const { everyone, roles, members } = mentions;
+
+	if (everyone) {
+		throw new TypeError('You can check only one person\'s points');
+	}
+
+	if (roles.size > 0) {
+		throw new TypeError('Mention one person, not their role(s).');
+	}
+
+	if (!members) {
+		return undefined;
+	}
+
+	if (members.size > 1) {
+		throw new TypeError('Mention only one person to view how many points they have.');
+	}
+
+	return members.first();
+};
+
 /**
  * 
  * @param {Discord.Client} client The Discord API client
@@ -16,14 +39,26 @@ exports.run = async (client, message) => {
 	}
 
 	let response;
+	let mentionedMember;
+
 	try {
-		const numPoints = await FeedbackPoint.count(client.database, member.id);
-		response = numPoints > 0 ?
-			`${member} has ${numPoints} ${numPoints > 1 ? 'points.' : 'point.'}` :
-			`${member} has no usable points, try giving some feedback by using \`${feedbackUsage}\``;
+		mentionedMember = getMentionedMember(message);
+	} catch (error) {
+		response = error.message;
 	}
-	catch (error) {
-		response = 'Something went wrong, please notify `@Staff`.';
+
+	if (!response) {
+		const memberId = mentionedMember ? mentionedMember.id : member.id;
+		const memberName = mentionedMember ? mentionedMember.displayName : member;
+		try {
+			const numPoints = await FeedbackPoint.count(client.database, memberId);
+			response = numPoints > 0 ?
+				`${memberName} has ${numPoints} ${numPoints > 1 ? 'points.' : 'point.'}` :
+				`${memberName} has no usable points${!mentionedMember ? `, try giving some feedback by using \`${feedbackUsage}\`` : '.'}`;
+		}
+		catch (error) {
+			response = 'Something went wrong, please notify `@Staff`.';
+		}
 	}
 
 	message.channel.send(response);
