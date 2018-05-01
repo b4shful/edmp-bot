f37671311db451f934f00545c3ae62243449eeea
Brian Uosseph <brian.uosseph@gmail.com>
2018-04-30 21:41:42 -0400

    Prettify filter files

--- filters/collab.js
+++ filters/collab.js
@@ -1,24 +1,25 @@
 /**
  * Clean up collab-bro
- * 
+ *
  * @param {Discord.Client} client The Discord API client
  * @param {Discord.Message} message A message on Discord
  */
-exports.run = async (client, message) => { 
-    let tags = ['need', 'style', 'daw', 'reference'];
-    let missing = [];
-    let idiotValidation = true;
+exports.run = async (client, message) => {
+	let tags = ["need", "style", "daw", "reference"];
+	let missing = [];
+	let idiotValidation = true;
 
-    for (let i = 0; i < tags.length; i++) {
-	if (!message.content.toLowerCase().includes(tags[i])) {
-	    missing.push(tags[i]);
+	for (let i = 0; i < tags.length; i++) {
+		if (!message.content.toLowerCase().includes(tags[i])) {
+			missing.push(tags[i]);
+		}
 	}
-    }
 
-    if (missing.length > 0) {
-	message.author.send(` I'm sorry, but your message in #collab-bro must contain the following fields: "need", "style", "DAW", "reference" (or "references"). Quotes are not necessary.
+	if (missing.length > 0) {
+		message.author
+			.send(` I'm sorry, but your message in #collab-bro must contain the following fields: "need", "style", "DAW", "reference" (or "references"). Quotes are not necessary.
 
-You were missing: ${missing.join(', ')}
+You were missing: ${missing.join(", ")}
 
 Please try like this:
 
@@ -28,14 +29,13 @@ DAW: Disableton Studio Tools 10.5.3
 References: http://soma.fm
 
 (If you actually post that, a Staff member will visit you with a ban-stick in hand.)`);
-    message.delete(0);
-    }
+		message.delete(0);
+	}
 };
-
 
 // Filters can have channelIDs as a single string or an array of strings.
 // This applies the filter to all IDs listed.
 exports.help = {
-    name: "collab",
-    channelID: ["284393945151307776", "414450318601093120"]
+	name: "collab",
+	channelID: ["284393945151307776", "414450318601093120"]
 };
