4d8d2fc90aa3bf06d3da6ce99c1660ba580f19fc
RobertRandolph (mbp) <audiolabs@gmail.com>
2018-05-14 12:40:23 -0400

    Added a fuzzy-search and cleaned up some code.

--- commands/selfrole.js
+++ commands/selfrole.js
@@ -1,69 +1,69 @@
-const Logger = require('../util/Logger');
+const Logger = require("../util/Logger");
+const stringSimilarity = require("string-similarity");
 
 const getRole = (guild, roleId) => {
-  const role = guild.roles.get(roleId);
+	const role = guild.roles.get(roleId);
 
-  if (!role) {
-    const error = new TypeError('Server is missing the ${role} role.');
-    Logger.error(error);
-    throw error;
-  }
+	if (!role) {
+		const error = new TypeError("Server is missing the ${role} role.");
+		Logger.error(error);
+		throw error;
+	}
 
-  return role;
-}
+	return role;
+};
 
 const assignRole = async (member, roleId) => {
-  try {
-    await member.addRole(roleiD);
-  }
-  catch (error) {
-    Logger.error(error);
+	try {
+		await member.addRole(roleId);
+	} catch (error) {
+		Logger.error(error);
 
-    throw new TypeError(`Unable to assign role ${roleId}`);
-  }
+		throw new TypeError(`Unable to assign role ${roleId}`);
+	}
 };
 
 exports.run = async (client, message, args, level) => {
-    const introChannelId = '371004421586550784';
-    const settings = client.config;
-    let roleFound = false;
-    let roleArray = [];
-    
-    if (message.channel.id === introChannelId) {
-	desiredRole = args.join(' ');
+	const introChannelId = "intro";
+	const settings = client.config;
+	let roleFound = false;
+	let roleArray = [];
 
-	for (var p in settings.selfRoles) {
-	    let roleCheck = settings.selfRoles[p].findIndex(x => x.toLowerCase() === desiredRole.toLowerCase());
+	if (message.channel.name === introChannelId) {
+		desiredRole = args.join("").trim();
 
-	    // findIndex returns -1 on no match otherwise the index is returned which is >=0
-	    if (if roleCheck > -1) {
-		let role = getRole(message.guild, p);
+		for (let p in settings.selfRoles) {
+			let roleCheck = stringSimilarity.compareTwoStrings(p, desiredRole);
 
-		assignRole(message.member, role);
-		
-		message.channel.send(`Role ${desiredRole} found with id ${p}, thank you for doing business.`);
-		roleFound = true;
-	    }
-	    
-	    roleArray.concat(settings.selfRoles[p]);
+			// string-similarity returns a value indicating the match's likelihood. I tested and 0.25 seems to be the sweet spot.
+			if (roleCheck > 0.25) {
+				let bestMatch = stringSimilarity.findBestMatch(desiredRole, Object.keys(settings.selfRoles));
+				let role = getRole(message.guild, settings.selfRoles[bestMatch.bestMatch.target]);
+
+				assignRole(message.member, role);
+
+				message.channel.send(`Role ${desiredRole} found! Thank you for doing business. Enjoy the server.`);
+				roleFound = true;
+			}
+		}
+		if (roleFound === false) {
+			message.channel.send(
+				`Role ${desiredRole} not found. Here are the possible options: ${Object.keys(settings.selfRoles).join(", ")}`
+			);
+		}
 	}
-	if (roleFound === false) {
-	    message.channel.send(`Role ${desiredRole} not found. Here are the possible options: ${roleArray.join(', ')}`);
-	}
-		
-    }
 };
 
 exports.conf = {
-  enabled: false,
-  guildOnly: true,
-  aliases: [],
-  permLevel: "User"
+	enabled: false,
+	guildOnly: true,
+	aliases: [],
+	permLevel: "User"
 };
 
 exports.help = {
-  name: "selfrole",
-  category: "Miscellaneous",
-  description: "Allows the user to set their role in the #intro room",
-  usage: "selfrole"
+	name: "selfrole",
+	category: "Miscellaneous",
+	description: "Allows the user to set their role in the #intro room",
+	usage: "selfrole"
 };
