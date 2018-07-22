90868bb201ea54ccdd09c249c3af7f359d5fbbc8
Brian Uosseph <brian.uosseph@gmail.com>
2018-07-22 11:53:37 -0400

    Add messageUpdate event listener logging

---
+++ events/messageUpdate.js
@@ -0,0 +1,25 @@
+const Logger = require('../util/Logger');
+
+module.exports = (_client, oldMessage, newMessage) => {
+	const { author, guild } = oldMessage;
+	Logger.debug(`${author.username}#${author.discriminator} (${author.id}) message was edited`);
+	if (author.bot || !guild || !guild.available) {
+		return;
+	}
+
+	const logChannel = guild.channels.find(({ type, name }) =>
+		type === 'text' && name === 'logs-general'
+	);
+
+	if (!logChannel) {
+		Logger.warn('Unable to find logging channel in server');
+		return;
+	}
+
+  const { channel, cleanContent: beforeContent } = oldMessage;
+  const { cleanContent: afterContent } = newMessage;
+	const { id, username, discriminator } = author;
+	const logMessage = `${username}#${discriminator} (\`${id}\`) message edited in **#${channel.name}**:\nBefore: ${beforeContent}\nAfter: ${afterContent}`;
+	Logger.log(logMessage);
+	logChannel.send(logMessage);
+};
