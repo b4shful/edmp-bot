c50b490ad8aadbb986a019d6e086ec2d5641e054
Brian Uosseph <brian.uosseph@gmail.com>
2018-07-22 03:33:10 -0400

    Update messageDelete event listener logging

--- events/messageDelete.js
+++ events/messageDelete.js
@@ -1,16 +1,24 @@
+const Logger = require('../util/Logger');
+
 module.exports = (_client, message) => {
 	const { author, guild } = message;
-	if (author.bot || !guild || !guild.available) return;
+	Logger.debug(`${author.username}#${author.discriminator} (${author.id}) message was deleted`);
+	if (author.bot || !guild || !guild.available) {
+		return;
+	}
 
 	const logChannel = guild.channels.find(({ type, name }) =>
-		type === 'text' && name === 'logs-general'
+		type === 'text' && name === 'modtalk-admin'
 	);
 
-	if (!logChannel) return;
+	if (!logChannel) {
+		Logger.warn('Unable to find logging channel in server');
+		return;
+	}
 
 	const { channel, cleanContent } = message;
 	const { id, username, discriminator } = author;
 	const logMessage = `${username}#${discriminator} (\`${id}\`) message deleted in **#${channel.name}**:\n${cleanContent}`;
-	console.log(logMessage);
+	Logger.log(logMessage);
 	logChannel.send(logMessage);
 };
