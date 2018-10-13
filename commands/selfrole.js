d1b6fb8e2649cb1d226a39ac1f2b4dbbf44f9ba6
Robert Randolph <robertrandolph@Roberts-iMac.home>
2018-10-13 17:49:16 -0400

    selfrole refactor

--- commands/selfrole.js
+++ commands/selfrole.js
@@ -23,33 +23,39 @@ const assignRole = async (member, roleId) => {
 	}
 };
 
+findRole = (settings, message, args) => {
+	desiredRole = args.join("").trim();
+
+	for (let p in settings.selfRoles) {
+		let roleCheck = stringSimilarity.compareTwoStrings(p, desiredRole);
+
+		// string-similarity returns a value indicating the match's likelihood. I tested and 0.25 seems to be the sweet spot.
+		if (roleCheck > 0.25) {
+			let bestMatch = stringSimilarity.findBestMatch(desiredRole, Object.keys(settings.selfRoles));
+			let role = getRole(message.guild, settings.selfRoles[bestMatch.bestMatch.target]);
+
+			return role;
+		}
+	}
+
+	return null;
+}
+
 exports.run = async (client, message, args, level) => {
 	const introChannelId = "intro";
 	const settings = client.config;
-	let roleFound = false;
-	let roleArray = [];
 
 	if (message.channel.name === introChannelId) {
-		desiredRole = args.join("").trim();
+		
+		let	role = findRole(settings, message, args);
 
-		for (let p in settings.selfRoles) {
-			let roleCheck = stringSimilarity.compareTwoStrings(p, desiredRole);
+		if (role) {
+			assignRole(message.member, role);
 
-			// string-similarity returns a value indicating the match's likelihood. I tested and 0.25 seems to be the sweet spot.
-			if (roleCheck > 0.25) {
-				let bestMatch = stringSimilarity.findBestMatch(desiredRole, Object.keys(settings.selfRoles));
-				let role = getRole(message.guild, settings.selfRoles[bestMatch.bestMatch.target]);
-
-				assignRole(message.member, role);
-
-				message.channel.send(
-					`Role ${bestMatch.bestMatch.target} found! Thank you for doing business. Enjoy the server.`
-				);
-				roleFound = true;
-				break;
-			}
-		}
-		if (roleFound === false) {
+			message.channel.send(
+				`Role ${role.name} found! Thank you for doing business. Enjoy the server.`
+			);
+		} else {
 			message.channel.send(
 				`Role "${args}" not found. Here are the possible options: ${Object.keys(settings.selfRoles).join(", ")}`
 			);
@@ -72,3 +78,4 @@ exports.help = {
 	description: "Allows the user to set their role in the #intro room",
 	usage: "selfrole"
 };
+
