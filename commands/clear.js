629281ffbd246b3f58e8258e7764434a9f28d76a
Robert Randolph <robertrandolph@Roberts-iMac.home>
2018-10-14 11:11:35 -0400

    refactored logger and improved clear

--- commands/clear.js
+++ commands/clear.js
@@ -1,56 +1,90 @@
-const Logger = require('../util/Logger');
+const Logger = require("../util/Logger");
 
 exports.run = async (client, message, args, level) => {
-    let {member, val} = getClearArgs(message, args);
+    let { member, val } = getClearArgs(message, args);
+    let logChannel;
 
-	if (val < 1) {
-        message.channel.send(`${message.member} You must supply a number of messages to delete that's greater than 0. \`${
-			help.usage
-		}\``);
+    if (val < 1) {
+        message.channel.send(
+            `${message.member} You must supply a number of messages to delete that's greater than 0. \`${help.usage}\``
+        );
+        return;
     }
-    
+
     if (!member && val < 1) {
-		message.channel.send(`${message.member} What do you think I'm gonna do? You didn't supply a member or a number of messages. I'm not psychic! \`${
-			help.usage
-		}\``);
-	}
+        message.channel.send(
+            `${
+                message.member
+            } What do you think I'm gonna do? You didn't supply a member or a number of messages. I'm not psychic! \`${
+                help.usage
+            }\``
+        );
+        return;
+    }
+
+    let modLog = Logger.getModLogger(message);
+
+    const logDelete = deleted => {
+        const { channel, cleanContent } = deleted;
+        const { id, username, discriminator } = message.author;
+
+        let logMessage = `${username}#${discriminator} deleted ${deleted.author.username}#${
+            deleted.author.discriminator
+        }'s message in **#${channel.name}**:\n${cleanContent} using clear`;
+        Logger.log(logMessage);
+
+        if (modLog) modLog.log(logMessage);
+    };
 
     if (!member) {
-        message.channel.bulkDelete(val);
-        Logger.log(`${message.author.username}#${message.author.discriminator} deleted the last ${val} messages in ${message.channel.name} using clear`)
-    } else if (member && val > 0) {
-        fetched = await message.channel.fetchMessages()
-            .then(messages => messages.filter(m => m.author.id === member.id))
-            .catch(console.error);
-            message.channel.bulkDelete(fetched);
-            Logger.log(`${message.author.username} deleted the last ${val} messages from ${member.username}#${message.author.discriminator} in ${message.channel.name} using clear`)
-            
-            // Delete the message triggering the clear. Debatable?
-            message.delete()
-                .then(msg => console.log(`Deleted message from ${msg.author.username}`))
+        // This is just really fucking stupid. Fuck javascript.
+        (async () => {
+            await message.channel
+                .fetchMessages({ limit: val })
+                .then(messages => {
+                    messages.forEach(msg => msg.delete().then(logDelete));
+                })
                 .catch(console.error);
+        })();
+        // Seriously dude. What The Fuck?
+
+
+    } else if (member && val > 0) {
+
+        // sigh
+
+        (async () => {
+            await message.channel
+                .fetchMessages()
+                .then(messages => messages.filter(m => m.author.id === member.id))
+                .then(messages => {
+                    messages.forEach(msg => msg.delete().then(logDelete));
+                })
+                .catch(console.error);
+        })();
+
     } else {
         throw new TypeError("No member and no value supplied. Disastah!");
     }
 };
 
 exports.conf = {
-	enabled: true,
-	guildOnly: true,
-	aliases: [],
-	permLevel: 'Staff'
+    enabled: true,
+    guildOnly: true,
+    aliases: [],
+    permLevel: "Staff"
 };
 
 exports.help = {
-	name: 'clear',
-	category: 'Moderation',
-	description: 'Clears the last n messages from a user',
-	usage: 'mute <user> <number>'
+    name: "clear",
+    category: "Moderation",
+    description: "Clears the last n messages from a user",
+    usage: "mute <user> <number>"
 };
 
 getClearArgs = (message, args) => {
     const mentionedMembers = message.mentions.members;
-    let retObj = {member: null, val: 0};
+    let retObj = { member: null, val: 0 };
 
     const firstMentionedMember = mentionedMembers.first();
 
@@ -63,4 +97,4 @@ getClearArgs = (message, args) => {
         retObj.member = firstMentionedMember;
         return retObj;
     }
-}
+};
--- util/Logger.js
+++ util/Logger.js
@@ -5,29 +5,45 @@ const chalk = require("chalk");
 const moment = require("moment");
 
 exports.log = (content, type = "log") => {
-	const timestamp = `[${moment().format("YYYY-MM-DD HH:mm:ss")}]:`;
-	switch (type) {
-		case "log": {
-			return console.log(`${timestamp} ${chalk.bgBlue(type.toUpperCase())} ${content} `);
-		}
-		case "warn": {
-			return console.log(`${timestamp} ${chalk.black.bgYellow(type.toUpperCase())} ${content} `);
-		}
-		case "error": {
-			return console.log(`${timestamp} ${chalk.bgRed(type.toUpperCase())} ${content} `);
-		}
-		case "debug": {
-			return console.log(`${timestamp} ${chalk.green(type.toUpperCase())} ${content} `);
-		}
-		case "cmd": {
-			return console.log(`${timestamp} ${chalk.black.bgWhite(type.toUpperCase())} ${content}`);
-		}
-		case "ready": {
-			return console.log(`${timestamp} ${chalk.black.bgGreen(type.toUpperCase())} ${content}`);
-		}
-		default:
-			throw new TypeError("Logger type must be either warn, debug, log, ready, cmd or error.");
-	}
+    const timestamp = `[${moment().format("YYYY-MM-DD HH:mm:ss")}]:`;
+    switch (type) {
+    case "log": {
+        return console.log(`${timestamp} ${chalk.bgBlue(type.toUpperCase())} ${content} `);
+    }
+    case "warn": {
+        return console.log(`${timestamp} ${chalk.black.bgYellow(type.toUpperCase())} ${content} `);
+    }
+    case "error": {
+        return console.log(`${timestamp} ${chalk.bgRed(type.toUpperCase())} ${content} `);
+    }
+    case "debug": {
+        return console.log(`${timestamp} ${chalk.green(type.toUpperCase())} ${content} `);
+    }
+    case "cmd": {
+        return console.log(`${timestamp} ${chalk.black.bgWhite(type.toUpperCase())} ${content}`);
+    }
+    case "ready": {
+        return console.log(`${timestamp} ${chalk.black.bgGreen(type.toUpperCase())} ${content}`);
+    }
+    default:
+        throw new TypeError("Logger type must be either warn, debug, log, ready, cmd or error.");
+    }
+};
+
+exports.getModLogger = message => {
+    const { author, guild } = message;
+    if (author.bot || !guild || !guild.available) {
+        return null; // should error
+    }
+
+    const logChannel = guild.channels.find(({ type, name }) => type === "text" && name === "logs-general");
+
+    if (!logChannel) {
+        exports.warn("Unable to find logging channel in server");
+        return null; // should error
+    } else {
+        return logChannel;
+    }
 };
 
 exports.error = (...args) => this.log(...args, "error");
